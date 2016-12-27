9ac74c31cfa9813913bc893a7dc96b6b4f17fbe4
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2016-12-27 22:16:51 +0100

    .

--- BtnAndLedWindow.cpp
+++ BtnAndLedWindow.cpp
@@ -2,6 +2,7 @@
 // Created by jpc on 21.12.16.
 //
 
+#include <iostream>
 #include <glibmm.h>
 #include <wiringPi.h>
 #include "BtnAndLedWindow.h"
@@ -10,7 +11,7 @@
 BtnAndLedWindow::BtnAndLedWindow(){
     set_title("02_BtnAndLed");
     m_timer = Glib::signal_timeout().connect(sigc::mem_fun(*this, &BtnAndLedWindow::timeout), 100);
-    m_label.set_markup("<span foreground='red' size='x-large'>OFF</span>");
+    setState(1);
     add(m_label);
     Gpio::setPinMode(1, INPUT);
     Gpio::controlPullUpPullDown(1, PUD_UP);
@@ -20,9 +21,15 @@ BtnAndLedWindow::~BtnAndLedWindow(){
     m_timer.disconnect();
 }
 
+void BtnAndLedWindow::setState(int state){
+	if (state != m_state){
+		m_state = state;
+		m_label.set_markup(m_state ? "<span foreground='red' size='x-large'>OFF</span>" : "<span foreground='green' size='x-large'>ON</span>");
+	}
+}
+
 bool BtnAndLedWindow::timeout() {
     int state = Gpio::readDigital(1);
-    if (state == 0){ // pressed
-    }
+    setState(state);
     return true;
 }
