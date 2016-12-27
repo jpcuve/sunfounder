ae6545123869da8e52623da59d65613e31c48a7a
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2016-12-27 11:20:13 +0100

    .

--- RgbLedWindow.cpp
+++ RgbLedWindow.cpp
@@ -7,7 +7,7 @@
 
 RgbLedWindow::RgbLedWindow() :
         m_box(Gtk::ORIENTATION_VERTICAL),
-        m_label("Color"),
+        m_label(),
         m_r(Gtk::ORIENTATION_HORIZONTAL),
         m_g(Gtk::ORIENTATION_HORIZONTAL),
         m_b(Gtk::ORIENTATION_HORIZONTAL) {
@@ -22,11 +22,12 @@ RgbLedWindow::RgbLedWindow() :
     m_b.set_range(0.0, 255.0);
     m_b.signal_value_changed().connect(sigc::mem_fun(*this, &RgbLedWindow::valueChanged));
     m_box.pack_start(m_b);
+    valueChanged();
     set_size_request(400, 50);
 }
 
 void RgbLedWindow::valueChanged() {
     char *buffer = new char[50]();
-    sprintf(buffer, "Color: %d,%d,%d", (int) m_r.get_value(), (int) m_g.get_value(), (int) m_b.get_value());
+    sprintf(buffer, "R:%d G:%d B:%d", (int) m_r.get_value(), (int) m_g.get_value(), (int) m_b.get_value());
     m_label.set_text(buffer);
 }
